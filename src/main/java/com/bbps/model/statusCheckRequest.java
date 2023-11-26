6058762a9de0e7ac38406e270a9786603e56d07d
pravinvignesh78 <pravinvignesh7@gmail.com>
2023-11-27 04:58:09 +0530

    BBPS-customer APP Validation Added

--- src/main/java/com/bbps/model/statusCheckRequest.java
+++ src/main/java/com/bbps/model/statusCheckRequest.java
@@ -16,6 +16,7 @@ public class statusCheckRequest implements Serializable {
 	private static final long serialVersionUID = 7948080969289899367L;
 
 	@NotBlank(message = "customerReqId is required")
+	@Pattern(regexp = "^[0-9]$",message="customerReqId should be numeric")
 	private String customerReqId;
 	private String reqType;
 	private String status;
